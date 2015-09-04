c6d58539f9e6f2b98f74155ae82fea72dfb267f0
dionysos83 <c.ohff@gmx.de>
2015-09-04 10:24:28 +0200

    Ruby Start

---
+++ studio_game.rb
@@ -0,0 +1,17 @@
+
+names = ['larry', 'curly', 'moe']
+health = 60
+puts "\n\n"
+	
+	puts "#{names[0]}'s health is #{health}"
+
+health = health * 3
+health = health / 9
+
+puts "\n\n"
+
+puts "Players:\n"
+names.each do |element|
+	puts element
+end
+
